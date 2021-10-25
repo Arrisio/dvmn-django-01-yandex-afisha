bc66be0fe0de08d46da6905e58938eac8d214bec
Dmitriy Suchkov <Account.log@yandex.ru>
2021-10-25 13:48:11 +0300

    ref: Place.description_short change type CharField>>TextField

--- places/models.py
+++ places/models.py
@@ -6,7 +6,7 @@ from yandex_afisha import settings
 
 class Place(models.Model):
     title = models.CharField(max_length=255, verbose_name='Название')
-    description_short = models.CharField(max_length=255, verbose_name='Краткое описание')
+    description_short = models.TextField(verbose_name='Краткое описание')
     description_long = HTMLField(verbose_name='Описание')
     lng = models.FloatField(verbose_name='Долгота')
     lat = models.FloatField(verbose_name='Широта')
