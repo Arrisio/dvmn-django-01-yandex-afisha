2917b26559820bab415b1034793821b171939b79
Dmitriy Suchkov <Account.log@yandex.ru>
2021-10-27 12:09:53 +0300

    fix: iss-2-03/04 static and media publishing

--- yandex_afisha/settings.py
+++ yandex_afisha/settings.py
@@ -131,14 +131,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / STATIC_URL]
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
 
 STATIC_ROOT = env.str("STATIC_ROOT", default=os.path.join(BASE_DIR, "static"))
 
 
-MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / MEDIA_URL
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR.joinpath('media')
 
 
 # Default primary key field type
